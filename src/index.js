69f915fbf7e32f90ba7f63bd48a64d393f184654
DBowen33 <42016383+DBowen33@users.noreply.github.com>
2022-04-11 16:38:26 -0500

    created App component and others

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import App from './components/app';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Create a new component. This component should produce some html
+const App = () => {
+    return <div>Hi!</div>;
+}
+// Take component's generated html and put on page
+ReactDOM.render(<App />, document.querySelector('.container')); //<App /> is instance of App class or component
